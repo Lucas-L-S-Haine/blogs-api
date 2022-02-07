5852144b38c96d2789203da14262bbab06a78e53
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-07 01:55:21 -0300

    Importa todas as rotas no index.

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const router = require('./routes');
+
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
+
+app.use(router);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
