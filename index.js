b1711108bc3e3388924ac069f2861194987e867e
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-12 08:26:51 -0300

    Volta a usar o arquivo de rotas anterior.

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require('express');
-const { User } = require('./models');
 
 const app = express();
 
-// const router = require('./routes');
+const router = require('./routes');
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
 app.use(express.json());
-// app.use(router);
-app.post('/user', async (req, res) => {
-  const { displayName, email, password, image } = req.body;
-  console.log(req.body);
-  try {
-    console.log(displayName);
-    await User.create({ displayName, email, password, image });
-    return res.status(201).json({ token: 'aslfkjasdf802' });
-  } catch (err) {
-    err.status = 403;
-    err.message = 'Desta vez num foi';
-    return res.status(err.status).send({ message: err.message });
-  }
-});
+
+app.use(router);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
