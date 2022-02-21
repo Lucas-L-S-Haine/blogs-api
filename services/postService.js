ea5f3a491a3a94c8d4561a352b1674e42aa124c2
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-02-21 00:20:23 -0300

    Adiciona transaction.

--- services/postService.js
+++ services/postService.js
@@ -1,8 +1,9 @@
 const {
+  sequelize,
   User,
   Categories: Category,
   BlogPosts: BlogPost,
-  // PostsCategories: PostCategory,
+  PostsCategories: PostCategory,
 } = require('../models');
 const { postValidate } = require('../validations');
 
@@ -10,15 +11,20 @@ const newError = (error) => (error);
 
 const createOne = async (userInput, userId) => {
   postValidate(userInput);
+  const transaction = await sequelize.transaction();
   const { categoryIds } = userInput;
   const categories = await Category.findAll({ attributes: ['id'] });
   const categoryList = categories.map((category) => category.dataValues.id);
-  const invalidIds = categoryIds
-    .filter((id) => categoryList.indexOf(id) === -1);
+  const invalidIds = categoryIds.filter((id) => categoryList.indexOf(id) === -1);
   if (invalidIds.length > 0) throw newError({ status: 400, message: '"categoryIds" not found' });
   const post = { ...userInput, userId };
-  console.log('categories\n', invalidIds);
-  const newPost = await BlogPost.create(post);
+  // const newPost = await BlogPost.create(post);
+  const newPost = await BlogPost.create(post, { transaction });
+  const { postId } = newPost;
+//  categoryIds
+//    .map((categoryId) => ({ postId, categoryId }))
+//    .forEach(async (obj) => { await PostCategory.create(obj, { transaction }); });
+  await transaction.commit();
   return newPost;
 };
 
