781ca4212e06da70c2220566d129f96c1392fadd
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-12 16:32:10 -0300

    Importa variável de ambiente PORT. Mudança de estilo.

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const app = express();
 const router = require('./routes');
 const errorMiddleware = require('./middlewares/errorHandler');
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Application online on port \x1b[03;94m${PORT}\x1b[00m!`));
 
 app.use(express.json());
 
 app.use(router);
 
 app.get('/', (request, response) => {
-  response.send();
+  response.send('Online!!!');
 });
 
 app.use(errorMiddleware);
