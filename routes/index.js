5a71ff739aa577ba6c5d04c670382338787c1955
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-12 18:08:41 -0300

    Desestrutura o método Router do módulo express.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const { Router } = require('express');
 
 const userRouter = require('./userRouter');
 const postRouter = require('./postRouter');
 const loginRouter = require('./loginRouter');
 const categoryRouter = require('./categoryRouter');
 
-const router = express.Router();
+const router = Router();
 
 router.use('/user', userRouter);
 router.use('/post', postRouter);
