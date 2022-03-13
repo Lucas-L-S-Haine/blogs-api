ab30e6c09c666434baa782acb067096315a5e97c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-13 01:38:40 -0300

    Refatora controlador de usuários.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,43 +1,29 @@
 const service = require('../services/userService');
 const { readToken } = require('../auth');
 
-const readOne = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await service.readOne(id);
-    return res.status(200).json(user);
-  } catch (err) {
-    return res.status(err.status).json({ message: err.message });
-  }
-};
+const readOne = (req, res, next) => service
+  .readOne(req.params.id)
+  .then((user) => res.status(200).json(user))
+  .catch(next);
 
-const readAll = async (req, res) => {
-  try {
-    const users = await service.readAll();
-    return res.status(200).json(users);
-  } catch (err) {
-    return res.status(err.status).json({ message: err.message });
-  }
-};
+const readAll = (req, res, next) => service
+  .readAll()
+  .then((users) => res.status(200).json(users))
+  .catch(next);
 
-const createOne = async (req, res) => {
-  try {
-    const user = req.body;
-    const token = await service.createOne(user);
-    return res.status(201).json({ token });
-  } catch (err) {
-    return res.status(err.status).send({ message: err.message });
-  }
-};
+const createOne = (req, res, next) => service
+  .createOne(req.body)
+  .then((token) => res.status(201).json({ token }))
+  .catch(next);
 
-const deleteOne = async (req, res) => {
+const deleteOne = async (req, res, next) => {
   try {
     const { authorization: token } = req.headers;
     const { email } = readToken(token);
     await service.deleteOne(email);
     return res.status(204).send();
   } catch (err) {
-    return res.status(err.status).send({ message: err.message });
+    next(err);
   }
 };
 
