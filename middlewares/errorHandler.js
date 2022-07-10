90b9ba3f6420abb33b89668ed25aafebfcc4f6e5
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-10 20:28:30 -0300

    style: add linebreak before res

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,2 +1,2 @@
-module.exports = (err, req, res, _next) => res
-  .status(err.status).send({ message: err.message });
+module.exports = (err, req, res, _next) =>
+  res.status(err.status).send({ message: err.message });
