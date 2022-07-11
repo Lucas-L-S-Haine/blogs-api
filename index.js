b442632eda9b71f61e16d7a810d5a81c6ffe98b8
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-10 21:20:52 -0300

    feat: handle 404 errors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const router = require('./routes');
 const errorMiddleware = require('./middlewares/errorHandler');
+const notFoundMiddleware = require('./middlewares/notFoundHandler');
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,5 +17,6 @@ app.get('/', (request, response) => {
 });
 
 app.use(errorMiddleware);
+app.use(notFoundMiddleware);
 
 app.listen(PORT, () => console.log(`Application online on port \x1b[03;94m${PORT}\x1b[00m!`));
