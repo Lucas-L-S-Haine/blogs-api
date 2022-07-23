4bf710fc9bf1326bfa9a6cd7b2ae355b63033c77
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-23 01:55:44 -0300

    feat: hash password when creating new user

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,4 @@
+const { hashSync: hash } = require('bcryptjs');
 const { User } = require('../models');
 const { newToken } = require('../auth');
 const { userValidate } = require('../validations');
@@ -21,6 +22,7 @@ const readAll = async () => {
 
 const createOne = async (user) => {
   userValidate(user);
+  const userData = { ...user, password: hash(user.password, 8) };
   if (await User.findOne({ where: { email: user.email } })) {
     const error = new Error();
     error.status = 409;
@@ -28,7 +30,7 @@ const createOne = async (user) => {
     throw error;
   }
   const token = newToken(user);
-  await User.create(user);
+  await User.create(userData);
   return token;
 };
 
