e909994155f7f4924e325dbcdc2c4b4077a3372f
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-23 01:56:38 -0300

    feat: compare password and hash

--- src/services/loginService.js
+++ src/services/loginService.js
@@ -1,20 +1,18 @@
+const { compareSync: compare } = require('bcryptjs');
 const { User } = require('../models');
 const { newToken } = require('../auth');
 const { loginValidate } = require('../validations');
 
 const login = async (loginData) => {
   loginValidate(loginData);
-  const user = await User.findAll({ where: {
-    email: loginData.email,
-    password: loginData.password,
-  } });
-  if (user.length === 0) {
-    const error = new Error();
-    error.status = 400;
-    error.message = 'Invalid fields';
-    throw error;
-  }
-  const { password: none, ...data } = user[0].dataValues;
+  const user = await User.findAll({ where: { email: loginData.email } });
+  const error = new Error();
+  error.status = 400;
+  error.message = 'Invalid fields';
+  if (user.length === 0) throw error;
+  const dbData = user[0].dataValues;
+  if (!compare(loginData.password, dbData.password)) throw error;
+  const { password: _, ...data } = dbData;
   const token = newToken(data);
   return token;
 };
