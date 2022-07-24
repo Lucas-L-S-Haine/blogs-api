00b4cae0feee4850b88195c46302745016642241
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-24 18:56:13 -0300

    refactor: update migrations
    Add SysAdmin user and change passwords into hashes.

--- src/seeders/20200812183211-Users.js
+++ src/seeders/20200812183211-Users.js
@@ -1,18 +1,30 @@
+const { hash } = require('bcryptjs');
+
 module.exports = {
   up: async (queryInterface, _Sequelize) => {
+    const root = await hash('root', 8);
+    const pass1 = await hash('123456', 8);
+    const pass2 = await hash('123456', 8);
     await queryInterface.bulkInsert('Users',
       [{
         id: 1,
-        displayName: 'Lewis Hamilton',
-        email: 'lewishamilton@gmail.com',
-        password: '123456',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
+        displayName: 'SysAdmin',
+        email: 'root@blogs.api',
+        password: root,
+        image: 'https://www.freelancinggig.com/blog/wp-content/uploads/2020/02/future-proof-sys-admin.png',
       },
       {
         id: 2,
+        displayName: 'Lewis Hamilton',
+        email: 'lewishamilton@gmail.com',
+        password: pass1,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
+      },
+      {
+        id: 3,
         displayName: 'Michael Schumacher',
         email: 'MichaelSchumacher@gmail.com',
-        password: '123456',
+        password: pass2,
         image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
       },
       ], { timestamps: false });
