3b5b519000e4f3ce581e2da7b35f5768a4cf28a3
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-25 14:19:39 -0300

    test: validate new categories

---
+++ tests/unit/validateCategory.spec.js
@@ -0,0 +1,17 @@
+const { resolve } = require('path');
+const validateCategory = require(
+  resolve(__dirname, '../..', 'src/validations/categoryValidate.js'));
+
+describe('Test category validation', () => {
+  it('doesn’t allow categories without name', () => {
+    const category = { name: '' };
+    const error = new Error();
+    error.message = '"name" is required';
+    expect(() => validateCategory(category)).toThrow(error);
+  });
+
+  it('doesn’t throw exception for valid category', () => {
+    const category = { name: 'Unit Tests' };
+    expect(() => validateCategory(category)).not.toThrow();
+  });
+});
