186d6bf7264f55075dcf717b8a2225ab19b0bf60
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-25 15:04:04 -0300

    refactor: add category ids

--- tests/unit/validatePost.spec.js
+++ tests/unit/validatePost.spec.js
@@ -4,14 +4,14 @@ const validatePost = require(
 
 describe('Test post validation', () => {
   it('doesn’t allow posts without title', () => {
-    const post = { content: 'I am writing a unit test' };
+    const post = { content: 'I am writing a unit test', categoryIds: [1, 2] };
     const error = new Error();
     error.message = '"title" is required';
     expect(() => validatePost(post)).toThrow(error);
   });
 
   it('doesn’t allow posts without content', () => {
-    const post = { title: 'Post Validation Unit Test' };
+    const post = { title: 'Post Validation Unit Test', categoryIds: [1, 2] };
     const error = new Error();
     error.message = '"content" is required';
     expect(() => validatePost(post)).toThrow(error);
@@ -21,6 +21,7 @@ describe('Test post validation', () => {
     const post = {
       title: 'Post Validation Unit Test',
       content: 'I am writing a unit test',
+      categoryIds: [1, 2],
     };
     expect(() => validatePost(post)).not.toThrow();
   });
