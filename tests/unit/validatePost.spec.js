29ebe0ae31fcef551c7adedba4505f4451716250
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-26 01:50:06 -0300

    test: validate created, updated and deleted posts

--- tests/unit/validatePost.spec.js
+++ tests/unit/validatePost.spec.js
@@ -1,6 +1,12 @@
 const { resolve } = require('path');
 const validatePost = require(
   resolve(__dirname, '../..', 'src/validations/postValidate.js'));
+const validateCreatePost = require(
+  resolve(__dirname, '../..', 'src/validations/createPostValidate.js'));
+const validateUpdatePost = require(
+  resolve(__dirname, '../..', 'src/validations/updatePostValidate.js'));
+const validateDeletePost = require(
+  resolve(__dirname, '../..', 'src/validations/deletePostValidate.js'));
 
 describe('Test post validation', () => {
   it('doesn’t allow posts without title', () => {
@@ -24,5 +30,83 @@ describe('Test post validation', () => {
       categoryIds: [1, 2],
     };
     expect(() => validatePost(post)).not.toThrow();
+  });
+});
+
+describe('Test validation for new posts', () => {
+  it('doesn’t allow posts without category ids', () => {
+    const post = {
+      title: 'Post Validation Unit Test',
+      content: 'I am writing a unit test',
+    };
+    const error = new Error();
+    error.message = '"categoryIds" is required';
+    expect(() => validateCreatePost(post)).toThrow(error);
+  });
+
+  it('doesn’t throw exception for valid post', () => {
+    const post = {
+      title: 'Post Validation Unit Test',
+      content: 'I am writing a unit test',
+      categoryIds: [1, 2],
+    };
+    expect(() => validateCreatePost(post)).not.toThrow();
+  });
+});
+
+describe('Test validation for updated posts', () => {
+  it('doesn’t allow any user to edit another user’s post', () => {
+    const post = {
+      title: 'Testing The API',
+      content: 'User Schumacher is editing posts',
+      id: 3,
+      userId: 4,
+    };
+    const error = new Error();
+    error.message = 'Unauthorized user';
+    expect(() => validateUpdatePost(post)).toThrow(error);
+  });
+
+  it('doesn’t allow changing categories', () => {
+    const post = {
+      title: 'Testing The API',
+      content: 'User Schumacher is editing posts',
+      id: 3,
+      userId: 3,
+      categoryIds: [1, 2, 3],
+    };
+    const error = new Error();
+    error.message = 'Categories cannot be edited';
+    expect(() => validateUpdatePost(post)).toThrow(error);
+  });
+
+  it('doesn’t throw exception for valid post', () => {
+    const post = {
+      title: 'Testing The API',
+      content: 'User Admin is editing posts from the database',
+      id: 3,
+      userId: 3,
+    };
+    expect(() => validateUpdatePost(post)).not.toThrow();
+  });
+});
+
+describe('Test validation for deleted posts', () => {
+  it('doesn’t allow any user to delete another user’s post', () => {
+    const input = {
+      id: 3,
+      userId: 4,
+    };
+    const error = new Error();
+    error.message = 'Unauthorized user';
+    expect(() => validateDeletePost(input)).toThrow(error);
+  });
+
+  it('doesn’t throw exception for valid post', () => {
+    const input = {
+      id: 3,
+      userId: 3,
+    };
+    expect(() => validateDeletePost(input)).not.toThrow();
   });
 });
