9605e0dc9984acc752cc72f2540601c2938c2668
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-07-29 13:50:14 -0300

    test: return one user
    First attempt at testing the services layer.

---
+++ tests/unit/services/__mocks__/User.js
@@ -0,0 +1,8 @@
+module.exports = {
+  findByPk: {
+    id: 3,
+    displayName: 'Michael Schumacher',
+    email: 'MichaelSchumacher@gmail.com',
+    image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
+  },
+};
---
+++ tests/unit/services/user.spec.js
@@ -0,0 +1,36 @@
+const { resolve } = require('path');
+const fs = require('fs/promises');
+
+const modelsDir = resolve(__dirname, '../../..', 'src/models');
+
+const User = {
+  findByPk: jest.fn(() => {
+    return new Promise((resolve) => {
+      resolve({
+        id: 3,
+        displayName: 'Michael Schumacher',
+        email: 'MichaelSchumacher@gmail.com',
+        image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
+      })
+    })
+  })
+}
+
+const servicesDir = resolve(__dirname, '../../..', 'src/services');
+const service = require(resolve(servicesDir, 'userService'));
+
+describe('Test user services', () => {
+  it('should return Schumacher', async () => {
+    const schumacher = {
+      id: 3,
+      displayName: 'Michael Schumacher',
+      email: 'MichaelSchumacher@gmail.com',
+      image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
+    };
+
+    const user = await service.readOne(3);
+    console.log(user);
+
+    expect(user).toEqual(schumacher);
+  })
+});
