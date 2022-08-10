e8655582cda03acd0e05e76581afb54d6c368f65
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-08-10 14:42:41 -0300

    style: pass object directly to mocked function

--- tests/unit/services/user.spec.js
+++ tests/unit/services/user.spec.js
@@ -10,14 +10,13 @@ User.findByPk = jest.fn();
 
 describe('Test user services', () => {
   beforeAll(() => {
-    const user = {
+    User.findByPk.mockReturnValue({
       id: 3,
       displayName: 'Michael Schumacher',
       email: 'MichaelSchumacher@gmail.com',
       image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
-    };
+    });
 
-    User.findByPk.mockReturnValue(user);
   });
 
   describe('readOne', () => {
