107571b78e8144a8b0025f6a41f47708088628a2
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-08-10 14:43:47 -0300

    test(unit): test createOne from user service

--- tests/unit/services/user.spec.js
+++ tests/unit/services/user.spec.js
@@ -1,4 +1,5 @@
 const { fail } = require('assert/strict');
+const jwt = require('jsonwebtoken');
 const models = require('../../../src/models');
 jest.mock('../../../src/models');
 const service = require('../../../src/services/userService');
@@ -7,6 +8,7 @@ const HTTPError = require('../../../src/utils/httpError');
 const { User } = models;
 
 User.findByPk = jest.fn();
+User.findOne = jest.fn();
 
 describe('Test user services', () => {
   beforeAll(() => {
@@ -17,6 +19,12 @@ describe('Test user services', () => {
       image: 'https://sportbuzz.uol.com.br/media/_versions/gettyimages-52491565_widelg.jpg',
     });
 
+    User.findOne.mockReturnValue({
+      id: 2,
+      displayName: 'Lewis Hamilton',
+      email: 'lewishamilton@gmail.com',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
+    });
   });
 
   describe('readOne', () => {
@@ -44,6 +52,59 @@ describe('Test user services', () => {
       const user = await service.readOne(3);
 
       expect(user).toEqual(schumacher);
+    });
+  });
+
+  describe('createOne', () => {
+    it('should throw error when email is already in use', async () => {
+      const hamilton = {
+        id: 2,
+        displayName: 'Lewis Hamilton',
+        email: 'lewishamilton@gmail.com',
+        password: '123456',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
+      };
+
+      const dbUser = {
+        dataValues: { ...hamilton },
+        get: () => hamilton,
+      };
+
+      User.findOne.mockReturnValueOnce(Promise.resolve(dbUser));
+
+      try {
+        await service.createOne(hamilton);
+        fail('function did not throw exception');
+      } catch(error) {
+        expect(error).toHaveProperty('message', 'User already registered');
+        expect(error).toBeInstanceOf(HTTPError);
+        expect(error).toHaveProperty('status', 409);
+      }
+    });
+
+    it('should return a token after user is successfully registered', async () => {
+      const secret = process.env.JWT_SECRET;
+      expect(secret).toBeTruthy();
+
+      User.findOne.mockReturnValueOnce(Promise.resolve(null));
+
+      const hamilton = {
+        id: 2,
+        displayName: 'Lewis Hamilton',
+        email: 'lewishamilton@gmail.com',
+        password: '123456',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Lewis_Hamilton_2016_Malaysia_2.jpg',
+      }
+
+      const token = await service.createOne(hamilton);
+      expect(token).toEqual(expect.any(String));
+
+      const payload = await jwt.verify(token, secret);
+      expect(payload).toHaveProperty('id', 2);
+      expect(payload).toHaveProperty('displayName', 'Lewis Hamilton');
+      expect(payload).toHaveProperty('email', 'lewishamilton@gmail.com');
+      expect(payload).toHaveProperty('image');
+      expect(payload).not.toHaveProperty('password');
     });
   });
 });
