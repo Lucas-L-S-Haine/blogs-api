7d8fa65e46cd24c9b79ae85841321eb0a6610736
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-09-14 02:47:43 -0300

    perf: improve default task performance
    Improve performance from 180ms to 90ms, effectively making it the same
    as calling "jake -t" from the command line.

--- tasks/jakeActions.js
+++ tasks/jakeActions.js
@@ -1,10 +1,64 @@
 const { spawn, execFile } = require('child_process');
 
-const { Task } = require('jake');
+const jake = require('jake');
 const jest = require('jest');
+const { green, grey } = require('chalk');
 
-async function runDefault() {
-  await spawn('jake', ['-t'], { stdio: 'inherit' });
+const { Task } = jake;
+
+function getFunctionParameters(action) {
+  let result = '';
+  const parameters = action.toString().split('(').at(1).split(')')[0];
+
+  if (parameters !== '') result += `[${parameters}]`;
+  return result;
+}
+
+function addTaskToList(leftSide, rightSide, stringSizes, parameters) {
+  const [line, label, size] = parameters;
+  leftSide.push(line);
+  rightSide.push(label);
+  stringSizes.push(size);
+}
+
+function printResultingTaskList(leftSide, rightSide, stringSizes, maximumStringSize) {
+  for (let index = 0; index < leftSide.length; index += 1) {
+    let result = leftSide[index];
+    const spaces = maximumStringSize - stringSizes[index] + 1;
+    result += Array(spaces).fill(' ').join('');
+    result += rightSide[index];
+
+    console.log(result);
+  }
+}
+
+function listTasks() {
+  const leftSide = [];
+  const rightSide = [];
+  const stringSizes = [];
+  let maximumStringSize = 0;
+  const { tasks } = jake.currentNamespace;
+
+  for (let index = 0, keys = Object.keys(tasks); index < keys.length; index += 1) {
+    const key = keys[index];
+    if (tasks[key].description != null) {
+      const { name, description, action } = tasks[key];
+      const parameters = getFunctionParameters(action);
+
+      const line = `jake ${green(name)}${parameters}`;
+      const label = `${grey('#')} ${grey(description)}`;
+      const size = line.length;
+
+      addTaskToList(leftSide, rightSide, stringSizes, [line, label, size]);
+      maximumStringSize = Math.max(maximumStringSize, size);
+    }
+  }
+
+  printResultingTaskList(leftSide, rightSide, stringSizes, maximumStringSize);
+}
+
+function runDefault() {
+  listTasks();
 }
 
 function handleLinter(error, stdout, _stderr) {
