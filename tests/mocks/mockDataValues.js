72d6cd7506820d7b7931ffe597ad6adf076a3428
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-09-20 21:28:26 -0300

    chore: create helper classes

--- tests/mocks/mockDataValues.js
+++ tests/mocks/mockDataValues.js
@@ -1,10 +1,39 @@
-class MockDataValues {
+const nameDescriptor = {
+  value: 'MockDataValues',
+  writable: false,
+  enumerable: false,
+  configurable: true,
+};
+
+class MockDataObject {
   constructor(dataValues) {
     this.dataValues = dataValues;
+    return this;
   }
 
-  get() {
-    return this.dataValues;
+  get(key) {
+    return this.dataValues[key] || this.dataValues;
+  }
+}
+
+class MockDataList {
+  constructor(dataValues) {
+    return dataValues.map((value) => new MockDataObject(value));
+  }
+}
+
+delete MockDataObject.name;
+delete MockDataList.name;
+Object.defineProperty(MockDataObject, 'name', nameDescriptor);
+Object.defineProperty(MockDataList, 'name', nameDescriptor);
+
+class MockDataValues {
+  constructor(dataValues) {
+    if (dataValues instanceof Array) {
+      return new MockDataList(dataValues);
+    } else {
+      return new MockDataObject(dataValues);
+    }
   }
 }
 
