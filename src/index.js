c383f141bcfba00f1477a050518d9ff734ba54e5
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-09-26 13:38:34 -0300

    refactor: change console message

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
-  console.log(`Application online on port \x1b[03;94m${PORT}\x1b[00m!`));
+  console.log(`Server listening on port \x1b[03;94m${PORT}\x1b[00m!`));
