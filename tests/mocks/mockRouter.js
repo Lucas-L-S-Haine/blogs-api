8ceb772d0ad1cc7f389d5a723c7f79749e94d346
Lucas L. S. Haine <lucaslshaine@gmail.com>
2022-11-27 19:11:57 -0300

    chore: create instance of class

--- tests/mocks/mockRouter.js
+++ tests/mocks/mockRouter.js
@@ -1,3 +1,5 @@
+const app = require('../../src/app');
+
 class MockRouter {
   constructor(app) {
     this._router = app._router;
@@ -6,5 +8,9 @@ class MockRouter {
   findRouter(path) {
   }
 }
+
+const router = new MockRouter(app);
+
+console.log(router._router);
 
 module.exports = MockRouter;
